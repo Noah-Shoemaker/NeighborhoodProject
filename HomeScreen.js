608a889c36ff0b04a34ad4b6860d05d8b0d2e007
njsho <noah.shoemaker@doane.edu>
2021-02-19 23:11:28 -0600

    finally fixed dependicies and bugs from moving project

--- HomeScreen.js
+++ HomeScreen.js
@@ -18,8 +18,6 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       marginTop: 40,
-      alignItems:'center',
-      alignContent:'center'
     },
     scrollContainer: {
       alignItems:'center',
